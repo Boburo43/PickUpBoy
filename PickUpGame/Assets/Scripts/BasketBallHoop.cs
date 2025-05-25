32e5d971b4b33bd68d3b5ec3cc84aae21438a864
Caspian2 <casa23@student.bth.se>
2025-05-25 22:26:25 +0200

    VFX when hit basketball
    Added a small vfx when you hit the basketballhoop :)

--- PickUpGame/Assets/Scripts/BasketBallHoop.cs
+++ PickUpGame/Assets/Scripts/BasketBallHoop.cs
@@ -1,9 +1,17 @@
 using UnityEngine;
-
+using UnityEngine.VFX;
 public class BasketBallHoop : MonoBehaviour
 {
+
+    [SerializeField] private Transform vfxSpawnPoint;
+    [SerializeField] private VisualEffect vfxPrefab;
+
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("HIt");
+        VisualEffect vfxInstance = Instantiate(vfxPrefab, vfxSpawnPoint.position, vfxSpawnPoint.rotation);
+
+        // Optionally, destroy it after a delay to clean up
+        Destroy(vfxInstance.gameObject, 2f);
     }
 }
